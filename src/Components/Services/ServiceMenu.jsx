b3a0fd775c40f240600784f399db391158033bf7
Eric Dimech <ericdimech@hotmail.com>
2021-02-20 22:52:13 -0500

    added service menu array to map over and populate service cards

---
+++ src/Components/Services/ServiceMenu.jsx
@@ -0,0 +1,40 @@
+import Personal from "../../Assets/Icons/personalized.png";
+import Companion from "../../Assets/Icons/companion.png";
+import Calendar from "../../Assets/Icons/calendar.png";
+import Shopping from "../../Assets/Icons/shopping.png";
+import Meals from "../../Assets/Icons/cooking.png";
+import Packing from "../../Assets/Icons/packing.png";
+
+export const ServiceMenu = [
+  {
+    image: Personal,
+    title: "Personalized Care",
+    cName: "services__menu-card",
+    // alt: "oijafjfafj"
+  },
+  {
+    image: Companion,
+    title: "Companionship & Assistance",
+    cName: "services__menu-card",
+  },
+  {
+    image: Calendar,
+    title: "Appointments",
+    cName: "services__menu-card",
+  },
+  {
+    image: Shopping,
+    title: "Errands & Outings",
+    cName: "services__menu-card",
+  },
+  {
+    image: Meals,
+    title: "Meals",
+    cName: "services__menu-card",
+  },
+  {
+    image: Packing,
+    title: "Home Organizing & Downsizing",
+    cName: "services__menu-card",
+  },
+];
--- src/Components/Services/Services.jsx
+++ src/Components/Services/Services.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import { ServiceMenu } from "./ServiceMenu";
 import "./Services.scss";
 
 function Services() {
   return (
     <div className="services" id="services">
       <h2 className="services__title">Services</h2>
+      <div className="services__menu-wrp">
+        {ServiceMenu.map((item, index) => {
+          return (
+            <div className={item.cName} key={index}>
+              <img
+                className="services__menu-img"
+                src={item.image}
+                alt="something"
+              />
+              <h3 className="services__menu-title">{item.title}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
