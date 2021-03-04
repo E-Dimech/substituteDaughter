0780dd4e5c244972f61ccf059d70e8c1d45beec7
Eric Dimech <ericdimech@hotmail.com>
2021-03-03 22:54:46 -0500

    Began building testimonials section

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -5,6 +5,17 @@ function Testimonials() {
   return (
     <div className="testimonials" id="testimonial">
       <h2 className="testimonials__title">Testimonials</h2>
+      <p>What would we do without you!</p>
+      <p>Couple Ages: 82 + 84</p>
+      <p>You're my angel</p>
+      <p>Widow Age: 90</p>
+      <p>Thank you for all that you do</p>
+      <p>Widow Age: 89</p>
+      <p>
+        Patricia is our driver, shopper, banker, organizer, cleaner, chef and
+        all the little things that can be a challenge for us
+      </p>
+      <p>Couple Ages: 85 + 87</p>
     </div>
   );
 }
