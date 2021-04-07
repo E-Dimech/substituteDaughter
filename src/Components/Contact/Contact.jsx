934e2302b46911c2ace4b1cb354bc3e1cc796e27
Eric Dimech <ericdimech@hotmail.com>
2021-04-07 17:27:02 -0400

    set up contact info section with image and info

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,34 @@
 import React from "react";
+import Eric from "../../Assets/Images/eric.jpg";
 import "./Contact.scss";
+import { Carousel } from "react-responsive-carousel";
 
 function Contact() {
   return (
     <div className="contact" id="contact">
       <h2 className="contact__title">Contact</h2>
+      <div className="contact__style-container">
+        <img className="contact__image" src={Eric} alt="it me" />
+
+        <div className="contact__card">
+          <div className="contact__card-info">
+            <p>
+              <i class="fas fa-phone"></i>+1 902 430 4660
+            </p>
+          </div>
+
+          <div className="contact__card-info">
+            <p>
+              <i class="far fa-envelope-open"></i>pdimech@gmail.com
+            </p>
+          </div>
+          <div className="contact__card-info">
+            <p className="contact__card-text">
+              Contact me for a free consultation
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
