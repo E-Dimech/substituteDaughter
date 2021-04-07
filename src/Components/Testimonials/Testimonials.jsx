73426f7e3936e7621dc3cdf532e23cc13fe1c59d
Eric Dimech <ericdimech@hotmail.com>
2021-04-07 12:22:51 -0400

    set up initial testimonials carousel feature

--- src/Components/Testimonials/Testimonials.jsx
+++ src/Components/Testimonials/Testimonials.jsx
@@ -1,138 +1,46 @@
 import React from "react";
 // import { MDBContainer, MDBRow, MDBCol, MDBIcon } from "mdbreact";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from "react-responsive-carousel";
 
 import "./Testimonials.scss";
 
 function Testimonials() {
   return (
-    // <div className="test">
-    //   <MDBContainer>
-    //     <section className="team-section text-center my-5">
-    //       <h2 className="h1-responsive font-weight-bold my-5">Testimonials</h2>
-    //       <p className="dark-grey-text w-responsive mx-auto mb-5">
-    //         Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-    //         error amet numquam iure provident voluptate esse quasi, veritatis
-    //         totam voluptas nostrum quisquam eum porro a pariatur veniam.
-    //       </p>
-
-    //       <MDBRow className="text-center">
-    //         <div className="test4">
-    //           <MDBCol md="4" className="mb-md-0 mb-5">
-    //             <div className="test3">
-    //               <div className="test2">
-    //                 <img
-    //                   src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg"
-    //                   alt=""
-    //                   className="rounded-circle z-depth-1 img-fluid"
-    //                 />
-    //               </div>
-    //               <h4 className="font-weight-bold dark-grey-text mt-4">
-    //                 Anna Deynah
-    //               </h4>
-    //               <h6 className="font-weight-bold blue-text my-3">
-    //                 Web Designer
-    //               </h6>
-    //               <p className="font-weight-normal dark-grey-text">
-    //                 <MDBIcon className="fa fa-quote-left pr-2" />
-    //                 Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-    //                 Quod eos id officiis hic tenetur quae quaerat ad velit ab
-    //                 hic tenetur.
-    //               </p>
-    //               <div className="orange-text">
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon far icon="star-half" />
-    //               </div>
-    //             </div>
-    //           </MDBCol>
-    //         </div>
-    //         <div className="test4">
-    //           <MDBCol md="4" className="mb-md-0 mb-5">
-    //             <div>
-    //               <div className="test2">
-    //                 <img
-    //                   src="https://mdbootstrap.com/img/Photos/Avatars/img%20(8).jpg"
-    //                   alt=""
-    //                   className="rounded-circle z-depth-1 img-fluid"
-    //                 />
-    //               </div>
-    //               <h4 className="font-weight-bold dark-grey-text mt-4">
-    //                 John Doe
-    //               </h4>
-    //               <h6 className="font-weight-bold blue-text my-3">
-    //                 Web Developer
-    //               </h6>
-    //               <p className="font-weight-normal dark-grey-text">
-    //                 <MDBIcon className="fa fa-quote-left pr-2" />
-    //                 Ut enim ad minima veniam, quis nostrum exercitationem ullam
-    //                 corporis suscipit laboriosam, nisi ut aliquid commodi.
-    //               </p>
-    //               <div className="orange-text">
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //               </div>
-    //             </div>
-    //           </MDBCol>
-    //         </div>
-    //         <div className="test4">
-    //           <MDBCol md="4">
-    //             <div>
-    //               <div className="test2">
-    //                 <img
-    //                   src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg"
-    //                   alt=""
-    //                   className="rounded-circle z-depth-1 img-fluid"
-    //                 />
-    //               </div>
-    //               <h4 className="font-weight-bold dark-grey-text mt-4">
-    //                 Maria Kate
-    //               </h4>
-    //               <h6 className="font-weight-bold blue-text my-3">
-    //                 Photographer
-    //               </h6>
-    //               <p className="font-weight-normal dark-grey-text">
-    //                 <MDBIcon className="fa fa-quote-left pr-2" />
-    //                 At vero eos et accusamus et iusto odio dignissimos ducimus
-    //                 qui blanditiis praesentium voluptatum deleniti atque
-    //                 corrupti.
-    //               </p>
-    //               <div className="orange-text">
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon icon="star" />
-    //                 <MDBIcon far icon="star" />
-    //               </div>
-    //             </div>
-    //           </MDBCol>
-    //         </div>
-    //       </MDBRow>
-    //     </section>
-    //   </MDBContainer>
-    // </div>
-    <div className="testimonials" id="testimonial">
+    <div className="testimonials">
       <h2 className="testimonials__title">Testimonials</h2>
-      <p>What would we do without you!</p>
-      <p>Couple Ages: 82 + 84</p>
-      <p>You're my angel</p>
-      <p>Widow Age: 90</p>
-      <p>Thank you for all that you do</p>
-      <p>Widow Age: 89</p>
-      <p>
-        Patricia is our driver, shopper, banker, organizer, cleaner, chef and
-        all the little things that can be a challenge for us
-      </p>
-      <p>Couple Ages: 85 + 87</p>
+      <Carousel
+        showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={4000}
+      >
+        <div className="test">
+          <h4>What would we do without you!</h4>
+          <p>Couple Ages: 82 + 84</p>
+        </div>
+
+        <div className="test">
+          <h4>You're my angel</h4>
+          <p>Widow Age: 90</p>
+        </div>
+
+        <div className="test">
+          <h4>Thank you for all that you do</h4>
+          <p>Widow Age: 89</p>
+        </div>
+        <div className="test">
+          <h4>
+            Patricia is our driver, shopper, banker, organizer, cleaner, chef
+            and all the little things that can be a challenge for us
+          </h4>
+          <p>Couple Ages: 85 + 87</p>
+        </div>
+      </Carousel>
     </div>
   );
 }
-
-//   );
-// }
 
 export default Testimonials;
